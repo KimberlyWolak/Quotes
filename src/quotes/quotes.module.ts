a31049c5e1e09a113154671464b47a571669f740
Kimberly Wolak <32212267+KimberlyWolak@users.noreply.github.com>
2023-06-10 18:17:32 +0000

    Update 4 files

--- src/quotes/quotes.module.ts
+++ src/quotes/quotes.module.ts
@@ -1,9 +1,15 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm'
 import { QuotesController } from './quotes.controller';
 import { QuotesService } from './quotes.service';
 
 @Module({
-  imports: [],
+  imports: [TypeOrmModule.forRoot({
+   type :"sqlite",
+   database: "quotesdb",
+   entities: [],
+   synchronize: true
+ })
   controllers: [QuotesController],
   providers: [QuotesService],
 })
